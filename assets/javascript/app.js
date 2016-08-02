afcdb3bd4d9e9b024d3a6147054f73dc4e577329
shaanobney <shaan.obney@gmail.com>
2016-08-02 11:15:02 -0700

    layout

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,7 +7,7 @@ var york = new Audio('york.mp3');
 var questBank = [
   {
     question: "The iconic mask this movie's villian wears was actually a Captain Kirk mask painted white",
-    choices: ['The Fog', 'Prince of Darkness', 'Halloween', 'The Thing', 'Vampires'],
+    ansPoss: ['The Fog', 'Prince of Darkness', 'Halloween', 'The Thing', 'Vampires'],
     answer: 2,
     image: 'assets/images/halloween.gif',
     wrongImage: 'assets/images/hallowrong.gif',
@@ -15,7 +15,7 @@ var questBank = [
 
   {
     question: "The model city used in this movie was later re-used in Blade Runner",
-    choices: ['Big Trouble In Little China', 'Assault On Precinct 13', 'Escape From New York', 'Escape from L.A.'],
+    ansPoss: ['Big Trouble In Little China', 'Assault On Precinct 13', 'Escape From New York', 'Escape from L.A.'],
     answer: 2,
     image: 'assets/images/newyork.gif',
     wrongImage: 'assets/images/yorkwrong.gif',
@@ -23,7 +23,7 @@ var questBank = [
 
   {
     question: "Which one of these films was originally conceived as a period Western?",
-    choices: ['Big Trouble In Little China', 'Assault On Precinct 13', 'Christine', 'The Thing'],
+    ansPoss: ['Big Trouble In Little China', 'Assault On Precinct 13', 'Christine', 'The Thing'],
     answer: 0,
     image: 'assets/images/china.gif',
     wrongImage: 'assets/images/chinawrong.gif',
@@ -31,7 +31,7 @@ var questBank = [
 
   {
     question: "This film is based on the sci-fi novel 'Who Goes There'",
-    choices: ['Dark Star','In the Mouth Of Madness', 'The Thing', 'Ghosts of Mars'],
+    ansPoss: ['Dark Star','In the Mouth Of Madness', 'The Thing', 'Ghosts of Mars'],
     answer: 2,
     image: "assets/images/thing.gif",
     wrongImage: 'assets/images/thething.gif',
@@ -39,7 +39,7 @@ var questBank = [
 
   {
     question:"The script for this film was chosen over E.T. by Columbia Pictures",
-    choices: ['Dark Star', 'Halloween', 'Ghosts of Mars', 'Starman'],
+    ansPoss: ['Dark Star', 'Halloween', 'Ghosts of Mars', 'Starman'],
     answer: 3,
     image: 'assets/images/starmanwr.gif',
     wrongImage: 'assets/images/starman.gif',
@@ -47,7 +47,7 @@ var questBank = [
 
   {
     question:"Based on a Stephen King novel, this movie features a jealous, haunted car",
-    choices: ['Elvis', 'Escape From L.A.', 'Vampires', 'Christine', 'The Ward'],
+    ansPoss: ['Elvis', 'Escape From L.A.', 'Vampires', 'Christine', 'The Ward'],
     answer: 3,
     image: 'assets/images/christine.gif',
     wrongImage: 'assets/images/christinewr.gif',
@@ -55,7 +55,7 @@ var questBank = [
 
   {
     question:"This film features a cameo by rocker Alice Cooper",
-    choices: ['In the Mouth of Madness', 'They Live', 'The Fog', 'The Ward', 'Prince of Darkness'],
+    ansPoss: ['In the Mouth of Madness', 'They Live', 'The Fog', 'The Ward', 'Prince of Darkness'],
     answer: 4,
     image: "assets/images/prince.gif",
     wrongImage: 'assets/images/darkness.gif',
@@ -63,7 +63,7 @@ var questBank = [
 
   {
     question:"The epic, iconic fight scene from this film took four weeks to rehearse",
-    choices: ['In the Mouth of Madness', 'They Live', 'The Fog', 'The Ward', 'Prince of Darkness'],
+    ansPoss: ['In the Mouth of Madness', 'They Live', 'The Fog', 'The Ward', 'Prince of Darkness'],
     answer: 1,
     image: "assets/images/they.gif",
     wrongImage: 'assets/images/live.gif',
@@ -74,8 +74,8 @@ var questBank = [
 $(document).ready(function() {
 	var counter = 16;
 	var selection = false;
-	var correctA = 0;
-	var incorrectA = 0;
+	var correctAns = 0;
+	var wrongAns = 0;
 	var Unanswered = 0;
 
  $('#startButton').on('click', function() {
@@ -105,25 +105,25 @@ function showQuestion(questionId) {
     $('#2').remove();
     $('#fatty').remove();
  		$('#mainEvent').html("<div class='question'>"+question.question+"</div>")
- 		for (var i = 0; i < question.choices.length; i++) {
+ 		for (var i = 0; i < question.ansPoss.length; i++) {
  			$('#mainEvent').append(
-			"<div class='choices' data-val='"+i+"'>"+question.choices[i]+"</button>"+"</div>");
+			"<div class='ansPoss' data-val='"+i+"'>"+question.ansPoss[i]+"</button>"+"</div>");
 	}
 	if (selection === false) {
-		$('.choices').on('click', function() {
+		$('.ansPoss').on('click', function() {
 			selection === true;
 			clearInterval(countDown);
 			if ($(this).data('val') === question.answer) {
 				rightAnswer(questionId, question);
-				correctA = correctA + 1;
+				correctAns = correctAns + 1;
 			} else {
 				wrongAnswer(questionId, question);
-				incorrectA = incorrectA + 1;
+				wrongAns = wrongAns + 1;
 			}
 		});
 	}
 	if (selection !== false) {
-		$('.choices').on('click', function() {
+		$('.ansPoss').on('click', function() {
 
       	})
 	}
@@ -150,7 +150,7 @@ function wrongAnswer(questionId, question) {
 	$('#announce').html(
 	"<div class='wrongAns'>" + "Wrong!" + "</div>")
 	$('#mainEvent').html(
-	"<div class='question'>" + "</div>" + "<div class='question'>" + "&nbsp;" + question.choices[question.answer] + "</div>" + "<img src='" + question.wrongImage + "'>")
+	"<div class='question'>" + "</div>" + "<div class='question'>" + "&nbsp;" + question.ansPoss[question.answer] + "</div>" + "<img src='" + question.wrongImage + "'>")
      setTimeout(function() {				     	        	    
      showQuestion(questionId + 1);
  },1500);
@@ -159,7 +159,7 @@ function wrongAnswer(questionId, question) {
 function outofTime(questionId, question) {
 	$('#announce').html(
 	"<div class='outtaTime'>" + "No Time Remains!" + "</div>")
-	$('#mainEvent').html("<div class='question'>" + "The correct answer is &nbsp;"+question.choices[question.answer] + "</div>" + "<img src='" + question.wrongImage + "'>")
+	$('#mainEvent').html("<div class='question'>" + "The correct answer is &nbsp;"+question.ansPoss[question.answer] + "</div>" + "<img src='" + question.wrongImage + "'>")
     setTimeout(function() {
     	showQuestion(questionId + 1);
     },2500);
@@ -167,8 +167,8 @@ function outofTime(questionId, question) {
 
 function score() {
 	$('#mainEvent').html(
- 	"<div class='question'>" + "RESULTS" + "</div>" + "<div class='question'>" + "Correct: &nbsp;" + correctA + "</div>"
- 	+ "<div class='question'>" + "Incorrect: &nbsp;" + incorrectA + "</div>"+ "<div class='question'>" + "Too Slow to Answer: &nbsp;" + Unanswered + "</div>"
+ 	"<div class='question'>" + "RESULTS" + "</div>" + "<div class='question'>" + "Correct: &nbsp;" + correctAns + "</div>"
+ 	+ "<div class='question'>" + "Incorrect: &nbsp;" + wrongAns + "</div>"+ "<div class='question'>" + "Too Slow to Answer: &nbsp;" + Unanswered + "</div>"
  	+ "<button type='button' class='restart'>Again?</button>")
 }
 
@@ -178,8 +178,8 @@ $('#mainEvent').on('click','.restart', function() {
 function restart() {
 	var counter = 16;
 	var selection = false;
-	var correctA = 0;
-	var incorrectA = 0;
+	var correctAns = 0;
+	var wrongAns = 0;
 	var Unanswered = 0;
   york.load();
 	showQuestion(0);
